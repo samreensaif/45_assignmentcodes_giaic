5703acd7959e25a0bad4c29ae826cfbe2650decd
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-01 12:40:51 +0500

    Add files via upload

---
+++ asgn8.js
@@ -0,0 +1,9 @@
+const repeat1 = "_".repeat(80);
+console.log(5 + 3);
+console.log(repeat1);
+console.log(11 - 3);
+console.log(repeat1);
+console.log(4 * 2);
+console.log(repeat1);
+console.log(16 / 2);
+console.log(repeat1);
