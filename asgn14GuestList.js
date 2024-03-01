577ac851dd519849d6d925e106c84702052cc55b
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-01 12:51:58 +0500

    Add files via upload

---
+++ asgn14GuestList.js
@@ -0,0 +1,3 @@
+var guest_list = ["Saif", "Naureen", "Zeenat", "Ayaz"];
+var inivitation = "I would like to invite you at Dinner on Monday 8:00 P.M. sharp";
+guest_list.forEach(function (abc) { return console.log("Hello, ".concat(abc), inivitation); });
---
+++ asgn14GuestList.ts
@@ -0,0 +1,6 @@
+let guest_list = ["Saif","Naureen","Zeenat","Ayaz"];
+
+let inivitation = "I would like to invite you at Dinner on Monday 8:00 P.M. sharp.";
+
+guest_list.forEach( abc => console.log(`Hello, ${abc}`, inivitation));
+
