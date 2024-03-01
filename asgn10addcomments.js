b52b6fbf208512eea1c23754becd7ec5726f6b29
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-01 12:44:38 +0500

    Add files via upload

---
+++ asgn10addcomments.js
@@ -0,0 +1,20 @@
+//// asgn_8: Number Eight: Write addition, subtraction, 
+//multiplication, and division operations that each result in
+// the number 8. Be sure to enclose your operations in print
+// statements to see the results. but it should looks different
+console.log(5 + 3);
+console.log("-----------------");
+console.log(11 - 3);
+console.log("-----------------");
+console.log(4 * 2);
+console.log("-----------------");
+console.log(16 / 2);
+console.log("-----------------");
+// asgn_8: Number Eight: Write addition, subtraction, 
+//multiplication, and division operations that each result in
+// the number 8. Be sure to enclose your operations in print
+// statements to see the results. 
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
