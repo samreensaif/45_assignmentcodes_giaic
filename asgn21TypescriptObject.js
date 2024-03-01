20f2322745f4d01275af58a6c5ed0907abbf61d9
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-01 13:14:03 +0500

    Add files via upload

---
+++ asgn21TypescriptObject.js
@@ -0,0 +1,6 @@
+var student = { name: "samreen",
+    fatherName: "Khurshid Anwar",
+    age: 42,
+    email: "samreenk81@hotmail.com",
+    schoolName: "Happy Home" };
+console.log(student);
---
+++ asgn21TypescriptObject.ts
@@ -0,0 +1,13 @@
+let student: {
+         name: string,
+         fatherName: string,
+         age: number,
+         email: string,
+         schoolName: String
+} = { name: "samreen",
+      fatherName: "Khurshid Anwar",
+       age: 42,
+       email: "samreenk81@hotmail.com",
+       schoolName: "Happy Home"};
+
+console.log(student);
