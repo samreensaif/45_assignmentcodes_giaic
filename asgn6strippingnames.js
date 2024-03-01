b241954bd2923e65e94a0ab5cc39f5ebb96c6f8d
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-01 12:34:59 +0500

    Add files via upload

---
+++ asgn6strippingnames.js
@@ -0,0 +1,4 @@
+var personName = "\n\t Samreen Saif\t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
---
+++ asgn6strippingnames.ts
@@ -0,0 +1,5 @@
+let personName: string = `\n\t Samreen Saif\t\n`;
+console.log(personName);
+
+var stripped = personName.trim();
+console.log(stripped);
