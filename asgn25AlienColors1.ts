af26b7fa90febf26851182f623e8455125d9b375
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-01 13:20:16 +0500

    Add files via upload

---
+++ asgn25AlienColors1.ts
@@ -0,0 +1,22 @@
+
+//Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+// Write an if statement to test whether the alien’s color is green. If it is, print
+//a message that the player just earned 5 points
+
+let alienColor: string = "red";
+
+if (alienColor==="red")
+{
+    console.log("the player just got 5 points ");
+}
+
+//Write one version of this program that passes the if test and another that
+//fails. (The version that fails will have no output.)
+
+ alienColor = "yellow";
+
+if (alienColor==="red")
+{
+console.log("-----");
+}
