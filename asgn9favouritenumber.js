3b514286accaebd594cdefed1faa45614517f8f2
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-01 12:42:23 +0500

    Add files via upload

---
+++ asgn9favouritenumber.js
@@ -0,0 +1,4 @@
+var favNumber = 100;
+var message = "my favourite number is:";
+console.log(" My Favourite Number is :", favNumber);
+console.log(message, favNumber);
---
+++ asgn9favouritenumber.ts
@@ -0,0 +1,8 @@
+let favNumber = 100;
+
+let message = "my favourite number is:";
+
+console.log(" My Favourite Number is :", favNumber);
+
+console.log(message , favNumber);
+
