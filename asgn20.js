688637d722797e6cdb35d71c7cf27dd8c3fe1ede
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-01 13:12:51 +0500

    Add files via upload

---
+++ asgn20.js
@@ -0,0 +1,3 @@
+var cityList = ["Karachi", "Lahore", "Islamabad", "Abbottabad", "Multan", "Faislabad"];
+console.log("\nPakistani Cities Name");
+cityList.forEach(function (abc) { return console.log(abc); });
---
+++ asgn20.ts
@@ -0,0 +1,4 @@
+let cityList= ["Karachi","Lahore","Islamabad","Abbottabad","Multan","Faislabad"];
+
+console.log("\nPakistani Cities Name");
+cityList.forEach(abc => console.log(abc));
