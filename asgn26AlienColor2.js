43c334d0270d07533bd31957ef8ff617f3de624f
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-01 14:42:00 +0500

    Add files via upload

---
+++ asgn26AlienColor2.js
@@ -0,0 +1,17 @@
+"use strict";
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and
+//write an if-else chain.
+let alienColor2 = "green";
+if (alienColor2 === "green") {
+    console.log("\nthe player just got 5 point");
+}
+else {
+    console.log("the player just got 10 points");
+}
+alienColor2 = "red";
+if (alienColor2 === "green") {
+    console.log("the player just got 5 point");
+}
+else {
+    console.log("\nthe player just got 10 points");
+}
---
+++ asgn26AlienColor2.ts
@@ -0,0 +1,35 @@
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and
+//write an if-else chain.
+
+//• If the alien’s color is green, print a statement that the player just earned 5 points
+// for shooting the alien.
+
+let alienColor2: string = "green";
+
+if (alienColor2==="green")
+
+{ 
+    console.log("\nthe player just got 5 point");
+}
+
+
+//• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+else
+{
+     console.log("the player just got 10 points");
+}
+
+//Write one version of this program that runs the if block and another that runs the else block.
+
+alienColor2 = "red"
+
+if (alienColor2==="green")
+
+{ 
+    console.log("the player just got 5 point");
+}
+
+else
+{
+     console.log("\nthe player just got 10 points");
+}
