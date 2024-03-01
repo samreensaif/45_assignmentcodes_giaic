978981b8a027a54386d08b042c7d73df70bb3e94
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-01 13:17:06 +0500

    Add files via upload

---
+++ asgn23Conditionaltest.js
@@ -0,0 +1,19 @@
+"use strict";
+// conditional test
+//test #1
+let car = "subaru";
+// = use to assign value
+//==use to check value
+// === use to check the value with the data type
+//TEST #1
+console.log("is car==`subaru`? i predict True");
+console.log(car == "subaru");
+//Test#2
+console.log("is car==`subaru`? i predict True");
+console.log(car == "subaru");
+//Test #3
+console.log("is car =`Honda`? i predict True");
+console.log(car == "Honda");
+//Test #4
+console.log("is car !=`Honda`? i predict True");
+console.log(car != "Honda");
---
+++ asgn23Conditionaltest.ts
@@ -0,0 +1,57 @@
+// conditional test
+
+//test #1
+
+let car: string = "subaru";
+
+  // = use to assign value
+  //==use to check value
+  // === use to check the value with the data type
+
+  //TEST #1
+console.log ("is car==`subaru`? i predict True");
+console.log(car =="subaru");
+
+
+//Test#2
+  
+console.log ("is car===`subaru`? i predict True");
+console.log(car ==="subaru");
+
+
+//Test #3
+ 
+console.log ("is car ==`Honda`? i predict False");
+console.log(car =="Honda");
+
+//Test #4
+
+console.log ("is car !=`Honda`? i predict True");
+console.log(car !="Honda");
+
+//Test#5
+console.log ("is car !==`ford`? i predict true");
+console.log(car !=="ford");
+
+// Test #6
+console.log ("is car.uppercase ==`SUBARU`? i predict false");
+console.log(car.toUpperCase() =="subaru");
+
+// Test #7
+console.log ("is car ==`SUBARU`? i predict false");
+console.log(car =="SUBARU");
+
+// Test #8
+console.log ("is car ===`SUBARU`? i predict false");
+console.log(car ==="SUBARU");
+
+// Test #9
+console.log ("is car ===`subaru`? i predict true");
+console.log(car ==="subaru");
+
+// Test #10
+console.log ("is car ==`plane`? i predict false");
+console.log(car =="plane");
+
+
+
