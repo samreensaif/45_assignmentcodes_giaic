4a1e25801bcd7d6be0b766fab310aba2928fc270
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-08 19:27:28 +0500

    Add files via upload

---
+++ asgn35Animals.js
@@ -0,0 +1,9 @@
+"use strict";
+//Animals: Think of at least three different animals that have a common characteristic.
+//Store the names of these animals in a list, and then use a for loop to
+// print out the name of each animal.
+let carnivoreAnimal = [`tiger`, `bear`, `wolf`, `lion`];
+for (let c_animal of carnivoreAnimal) {
+    console.log(`A ${c_animal} is a carnivore.`);
+}
+console.log(`Any of these animals is dangerous for human beings as they like to eat meat.`);
---
+++ asgn35Animals.ts
@@ -0,0 +1,15 @@
+//Animals: Think of at least three different animals that have a common characteristic.
+
+//Store the names of these animals in a list, and then use a for loop to
+// print out the name of each animal.
+let carnivoreAnimal: string[]= [`tiger`,`bear`,`wolf`,`lion`];
+
+for(let c_animal of carnivoreAnimal)
+{
+
+        console.log(`A ${c_animal} is a carnivore.`)
+}
+
+console.log(`Any of these animals is dangerous for human beings as they like to eat meat.`);
+
+
