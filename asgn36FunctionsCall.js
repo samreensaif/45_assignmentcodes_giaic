08276a34b21b29ed5d7aa4b0ddb1e4e0124f0510
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-08 20:23:45 +0500

    Add files via upload

---
+++ asgn36FunctionsCall.js
@@ -0,0 +1,15 @@
+// 36. T-Shirt: Write a function called make_shirt() that accepts a size and the text of a 
+// message that should be printed on the shirt. The function should print a sentence summarizing
+// the size of the shirt and the message printed on it.
+// Call the function.
+function T_Shirt(size, msg) {
+    console.log("\nSize of T-Shirt:" + size);
+    console.log(msg);
+}
+T_Shirt(28, "I AM PAKISTANI\n");
+//another example of Function declaration
+function test_param(n1, s1) {
+    console.log(n1);
+    console.log(s1);
+}
+test_param(123, "this is a string");
