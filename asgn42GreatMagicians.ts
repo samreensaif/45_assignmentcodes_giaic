84d8eead41fc599b640a12982e5a0c8819505ef9
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-14 01:30:27 +0500

    Add files via upload

---
+++ asgn42GreatMagicians.ts
@@ -0,0 +1,31 @@
+/*42. Great Magicians: Start with a copy of your program from Exercise 41.
+Write a function called make_great() that modifies the array of magicians by adding
+the phrase the Great to each magician’s name. Call show_magicians() to
+see that the list has actually been modified*/
+
+
+//function show_magician to print the original array 
+
+let magicians_name: string[] = ["David Blaine","Doug Henning", "Criss Angel"];
+function show_magicians(magician :string[] ) 
+{  
+for (let i of magician) {
+       console.log(`${i}`);
+}
+}
+
+console.log(`\n original array`);
+show_magicians(magicians_name);
+
+// Makes another function make_great to modify the original array by using map() function then print
+
+function make_great (abc:string[]) 
+{
+       let modified_array = abc.map((abc) =>  `The Great, ${abc}`);
+       return modified_array;         
+}
+let f = make_great(magicians_name)
+
+console.log(`\n modified array`);
+show_magicians(f);
+
