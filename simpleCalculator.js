32cd19f9a581aa0a57fedb93ec539a3bd27b7df8
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-16 22:54:05 +0500

    Add files via upload

---
+++ simpleCalculator.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+console.log("\nSIMPLE CALCULATOR\n");
+const result = await inquirer.prompt([
+    { msg: "Enter 1st #:", type: "number", name: "fno" },
+    { msg: "ENter 2nd #:", type: "number", name: "sno" },
+    {
+        msg: "Select Operators",
+        type: "list",
+        name: "operators",
+        choices: [
+            "Addition",
+            "Subtraction",
+            "Multiplication",
+            "Division",
+            "Modulus",
+        ],
+    },
+]);
+if (result.operators === "Addition") {
+    console.log("Addition Result is:", result.fno + result.sno);
+}
+else if (result.operators === "Subtraction") {
+    console.log("Subtraction Result is:", result.fno - result.sno);
+}
+else if (result.operators === "Multiplication") {
+    console.log("Multiplication Result is:", result.fno * result.sno);
+}
+else if (result.operators === "Division") {
+    console.log("Division Result is:", result.fno / result.sno);
+}
+else if (result.operators === "Modulus") {
+    console.log("Modulus Result is:", result.fno % result.sno);
+}
